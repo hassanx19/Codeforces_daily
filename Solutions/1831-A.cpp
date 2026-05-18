22a912cb2b57da425f476c1b74f873ff680321bf
Hassan <hassankx19@gmail.com>
2026-05-18 19:52:41 +0530

    Add solution for problem 1831-A
    Implement solution for problem 1831-A with input handling and output generation.

---
+++ Solutions/1831-A.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc.h>
+using namespace std;
+int main(){ 
+   int t;
+   cin>>t;
+   while(t--){
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i = 0 ; i < n ; i++){
+        cin>>a[i];
+    }
+    vector<int> b(n);
+    for(int i = 0 ; i < n ; i++){
+        b[i] = n+1 - a[i];
+        cout<<b[i]<<" ";
+    }
+    cout<<endl;
+   }
+    return 0;
+}
